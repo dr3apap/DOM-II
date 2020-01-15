931834f0911aa8dda94752af1be94a06c3aeee0b
Adebola  Adesina <adebolaadesina@adebolas-mbp.home>
2020-01-14 21:32:17 -0800

    initial

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
 // Your code goes here
+const myNav = document.querySelector(".nav-container");
+
+myNav.addEventListener('mouseleave', () => {
+    myNav.style.backgroundColor = "teal";
+});
+
+const heading = document.querySelector('h1')
+
+heading.addEventListener('mouseover', () => {
+
+    heading.style.fontSize = '7rem'
+})
+
+document.querySelectorAll('.nav-link').forEach(element => {
+
+    element.addEventListener('mouseenter', () => {
+        element.style.transform = 'rotateY(-360deg)'
+        element.style.transitionDuration = '1000ms'
+        element.style.color = 'orange'
+    })
+});
+
+const topImg = document.querySelector('img')
+
+window.addEventListener('resize', () => {
+    topImg.src = 'https://images.unsplash.com/photo-1544448045-ef21e76c8221?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
+
+})
+
+document.querySelectorAll('p').forEach(text => {
+    console.log(text)
+    text.addEventListener('click', (event) => {
+        alert('You click on some text')
+    })
+})
+
+
+window.addEventListener('load', (event) => {
+    alert('Page is fully loaded')
+
+})
+
+let theWheel = document.querySelector("body");
+theWheel.addEventListener("wheel", () => {
+    theWheel.style.backgroundColor = "olive";
+});
+
+// document.querySelectorAll('.destination').forEach(item => {
+//     console.log(destination)
+//     item.addEventListener('doublcick, () => {
+//         item.style.backgroundColor = 'tomato'
+//     })
+// });
+const mouseFocus = document.querySelector('.footer')
+
+mouseFocus.addEventListener('mousemove', () => {
+
+    mouseFocus.style.backgroundColor = "grey"
+
+}) 
---
+++ stretch-assignment/index.js
@@ -0,0 +1,5 @@
+const myNav = document.querySelector("h1");
+console.log(myNav);
+// myNav.addEventListener("mouseenter", () => {
+//   myNav.style.backGroundColor = "teal";
+// });
